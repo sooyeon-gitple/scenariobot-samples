c79db2d7be4e9e50dfa0675fc175247cb4834a44
sooyeon <sooyeon.cha@gitple.com>
2022-07-18 18:02:32 +0900

    patientTest

--- patientTest/index.js
+++ patientTest/index.js
@@ -1,16 +1,19 @@
 
 const TEST_RESULTS = ['1', '2', '3', '4', '5'];
-let type =  'start'//'<%= type %>';
+// let type =  'start' //'<%= type %>';
+let isShared = false;
 
 let resultPoint = 0;
+let testResult = null; // Number(1 ~ 5)
+// let ogUrl =  null;
+// let ogTitle =  'null';
+// let ogDesc = null;
 
 
 
-if (type !== 'start' && !TEST_RESULTS.includes(type)) {
-
-	window.location = './index.html';
-  // window.location.href =  window.location.origin + '/static/patient_test';
-}
+// if (isShared) {
+//   // window.location.href =  window.location.origin + '/static/patient_test';
+// }
 
 if (window.addEventListener) {
   window.addEventListener('load', windowLoaded, false);
@@ -18,21 +21,6 @@ if (window.addEventListener) {
   window.attachEvent('onload', windowLoaded);
 } else {
   window.onload = windowLoaded;
-}
-
-function getUrlParam(key) {
-	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
-	return urlParams.get(key);
-}
-
-function windowLoaded() {
-	type = getUrlParam('type') || 'start';
-	console.log('>>>type :', type);
-
-  if (type !== 'start') {
-    showResult(type);
-  }
 }
 
 window.GitpleConfig = {
@@ -63,6 +51,41 @@ function openGitple() {
   }
 }
 
+function getUrlParam(key) {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	return urlParams.get(key);
+}
+
+function windowLoaded() {
+
+	isShared = getUrlParam('type') === 'share';
+  testResult = getUrlParam('result');
+  console.log(1111)
+	console.log('>>>type :', isShared);
+  console.log('>>>testResult :', testResult);
+
+
+  if (isShared && testResult) {
+    setOpenGraphContents();
+    // const { ogTitle, ogDesc, ogImg, ogUrl } = getOpenGraphText(testResult);
+    // console.log( ogTitle,'|', ogDesc,'|', ogImg,'|', ogUrl )
+
+    // const head = document.querySelector('head');
+    // const metaUrl = document.createElement('meta')
+    // metaUrl.setAttribute('property', 'og:url')
+    // metaUrl.setAttribute('content', ogUrl);
+
+    // head.appendChild(metaUrl);
+
+
+  }
+  if (testResult) {
+    showResult(testResult);
+  }
+}
+
+
 function getTestResult(result) {
   let testResult;
 
@@ -91,7 +114,7 @@ function getTestResult(result) {
 
 function copyPageUrl() {
   const tempInput = document.createElement('input'),
-        text = window.location.href + '?type=share';
+        text = window.location.href + '?type=share' + '&result='+ testResult;
 
   document.body.appendChild(tempInput);
 
@@ -105,7 +128,14 @@ function copyPageUrl() {
 }
 
 function goToMain() {
-	window.location = './index.html';
+  isShared = getUrlParam('type') === 'share';
+  if (isShared) {
+    window.location = './index.html?type=share';
+  } else {
+    './index.html';
+  }
+
+  console.log(4444, isShared, testResult)
   // const url = window.location.origin + '/static/patient_test';
   // window.location = url;
 }
@@ -115,20 +145,90 @@ function goToGitpleChat() {
   window.open(url);
 }
 
-function setOpenGraphText(title, description) {
-  const ogTitle = document.querySelector('meta[property="og:title"]'),
-        ogDesc = document.querySelector('meta[property="og:description"]');
 
-  ogTitle.setAttribute('content', title);
-  ogDesc.setAttribute('content', description);
+function setOpenGraphContents() {
+  const { ogUrl, ogTitle, ogImg, ogDesc } = getOpenGraphText(testResult);
+  console.log( ogTitle,'|', ogDesc,'|', ogImg,'|', ogUrl )
+  const head = document.querySelector('head');
+
+  const metaUrl = document.createElement('meta')
+  metaUrl.setAttribute('property', 'og:url')
+  metaUrl.setAttribute('content', ogUrl);
+
+  const metaTitle = document.createElement('meta')
+  metaTitle.setAttribute('property', 'og:title')
+  metaTitle.setAttribute('content', ogTitle);
+
+  const metaImage = document.createElement('meta')
+  metaImage.setAttribute('property', 'og:image')
+  metaImage.setAttribute('content', ogImg);
+
+  const metaDescription = document.createElement('meta')
+  metaDescription.setAttribute('property', 'og:description')
+  metaDescription.setAttribute('content', ogDesc);
+
+  head.appendChild(metaUrl);
+  head.appendChild(metaTitle);
+  head.appendChild(metaImage);
+  head.appendChild(metaDescription);
+}
+
+function setOpenGraphText(title, description) {
+  // const ogTitle = document.querySelector('meta[property="og:title"]'),
+  //       ogDesc = document.querySelector('meta[property="og:description"]');
+
+  // ogTitle.setAttribute('content', title);
+  // ogDesc.setAttribute('content', description);
 }
 
 function setOpenGraphImg(testResult) {
-  const ogImg = document.querySelector('meta[property="og:title"]');
-  ogImg.setAttribute('src', window.location.origin + '/static/img/og_' + testResult + '.png');
+  // const ogImg = document.querySelector('meta[property="og:title"]');
+  // ogImg.setAttribute('src','./results/images/og_' + testResult + '.png');
+}
+
+
+Gitple('onMessage', (message) => {
+  if (message.type === "resetResult") { // reset point
+    resultPoint = Number(message.data.point);
+  } else if (message.type === "add") {  // add point
+    resultPoint += Number(message.data.point);
+  } else if (message.type === "getTestResult") {
+    testResult = getTestResult(resultPoint);
+    showResult(testResult);
+  }
+})
+
+function showResult(testResult) {
+  console.log(2222, testResult)
+  this.setOpenGraph(testResult);
+
+  const startBlock = document.querySelector('.bad-customer-start');
+  if (startBlock) {
+    startBlock.style.display = 'none';
+  }
+
+  const resultContainer = document.querySelector('.result-container'),
+	resultImage = document.querySelector('.result-image > img');
+	resultImage.setAttribute('src', './results/images/test_result_' + testResult + '.png');
+	//   resultImage.setAttribute('src', window.location.origin + '/static/img/test_result_' + testResult + '.png');
+  resultContainer.classList.add('show');
+
+  const resultText = document.querySelector('.result-' + testResult);
+  resultText && resultText.classList.add('show');
+
+  const testButton = document.getElementById('tryTestButton');
+  if (isShared) {
+    testButton.innerText = ' 나도 테스트 해보기'
+  } else {
+    testButton.innerText = '다시 테스트 하기'
+  }
+
+  Gitple('close');
 }
 
 function setOpenGraph(testResult){
+  console.log(3333, testResult)
+
   switch(testResult) {
     case '1':
       setOpenGraphText('당신의 인내력: 세살아기 등급', '이러한 인내력으로는 세상을 살아갈 수 없어요!');
@@ -150,36 +250,56 @@ function setOpenGraph(testResult){
   setOpenGraphImg(testResult);
 }
 
-Gitple('onMessage', (message) => {
-  if (message.type === "resetResult") { // reset point
-    resultPoint = Number(message.data.point);
-  } else if (message.type === "add") {  // add point
-    resultPoint += Number(message.data.point);
-  } else if (message.type === "getTestResult") {
-    const testResult = getTestResult(resultPoint);
-    showResult(testResult);
-    // window.location = './results/' + testResult + '.html';
-    // window.location = window.location.origin + '/static/patient_test/' + testResult;
+
+const getOpenGraphText = (id /*: string = ''*/) => {
+
+  console.log('>>>DDD',id)
+
+  const baseSrc =  './results/images';//'https://api-sample.gitple.io/static/img',
+        baseUrl =  './results';//'https://api-sample.gitple.io/static/patient_test';
+
+  switch(id) {
+    case '1':
+      return {
+        ogTitle: '당신의 인내력: 세살아기 등급',
+        ogDesc: '이러한 인내력으로는 세상을 살아갈 수 없어요!',
+        ogImg: `${baseSrc}/og_${id}.png`,
+        ogUrl: `${baseUrl}?result=1`
+      };
+    case '2':
+      return {
+        ogTitle: '당신의 인내력: 만년다이어터 등급',
+        ogDesc: '이정도면… 유리멘탈…',
+        ogImg: `${baseSrc}/og_${id}.png`,
+        ogUrl: `${baseUrl}?result=2`
+      };
+    case '3':
+      return {
+        ogTitle: '당신의 인내력: 어린이 등급',
+        ogDesc: '이 험난한 사회, 어떻게 버티시려구요?',
+        ogImg: `${baseSrc}/og_${id}.png`,
+        ogUrl: `${baseUrl}/${id}`
+      };
+    case '4':
+      return {
+        ogTitle: '당신의 인내력: 회사원 등급',
+        ogDesc: '사회생활하는데 문제는 없겠는걸요?',
+        ogImg: `${baseSrc}/og_${id}.png`,
+        ogUrl: `${baseUrl}/${id}`
+      };
+    case '5':
+      return {
+        ogTitle: '당신의 인내력: 부처상 등급',
+        ogDesc: '당신은 그 어떤 진상도 버텨낼 수 있으리!',
+        ogImg: `${baseSrc}/og_${id}.png`,
+        ogUrl: `${baseUrl}/${id}`
+      };
+    default:
+      return {
+        ogTitle: '인내력 테스트: 나의 인내력은 어느 수준일까?',
+        ogDesc: '진상 고객과 대화, 어디까지 버틸 수 있니?',
+        ogImg: `${baseSrc}/og_main.png`,
+        ogUrl: `${baseUrl}`
+      };
   }
-})
-
-function showResult(testResult) {
-  this.setOpenGraph(testResult);
-
-  const startBlock = document.querySelector('.bad-customer-start');
-  if (startBlock) {
-    startBlock.style.display = 'none';
-  }
-
-  const resultContainer = document.querySelector('.result-container'),
-	resultImage = document.querySelector('.result-image > img');
-	console.log('here',  testResult )
-	resultImage.setAttribute('src', './results/images/test_result_' + testResult + '.png');
-	//   resultImage.setAttribute('src', window.location.origin + '/static/img/test_result_' + testResult + '.png');
-  resultContainer.classList.add('show');
-
-  const resultText = document.querySelector('.result-' + testResult);
-  resultText && resultText.classList.add('show');
-
-  Gitple('close');
 }
